2db4215b3ba8e169e35aad59c2af0ffec52520c4
Narendra KUMAR <narendra.kumar@amadeus.com>
2023-08-15 17:21:25 -0400

    indentation

--- 16_writing_tests/src/lib.rs
+++ 16_writing_tests/src/lib.rs
@@ -2,19 +2,19 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-struct Rectangle{
+struct Rectangle {
     width: u32,
     height: u32,
 }
 
-impl Rectangle{
-    fn can_hold(self ,other: &Rectangle)-> bool{
+impl Rectangle {
+    fn can_hold(self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
 }
 
-pub fn add_two(num: u32)-> u32{
-    2+num
+pub fn add_two(num: u32) -> u32 {
+    2 + num
 }
 
 #[cfg(test)]
@@ -25,20 +25,20 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
-   }
+    }
 
     #[test]
-    fn exploration(){
-        assert_ne!(4,5);
+    fn exploration() {
+        assert_ne!(4, 5);
     }
 
     // #[test]
     // fn hit_panic(){
     //     panic!("panicking test");
     // }
-#[test]
-    fn can_hold_rectangles(){
-        let rect1 = Rectangle{
+    #[test]
+    fn can_hold_rectangles() {
+        let rect1 = Rectangle {
             width: 1,
             height: 2,
         };
@@ -51,13 +51,13 @@ mod tests {
         assert!(rect2.can_hold(&rect1));
     }
 
-#[test]
-    fn add_two_to_two(){
+    #[test]
+    fn add_two_to_two() {
         assert_eq!(4, add_two(2));
     }
-    
-#[test]
-    fn result_contains_carol(){
+
+    #[test]
+    fn result_contains_carol() {
         let result = "it contains ca!";
         assert!(result.contains("carol"), "Result did not contain 'carol'");
     }
